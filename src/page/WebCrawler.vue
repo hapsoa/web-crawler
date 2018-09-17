ec5103a3ef176f39877a1b5470d5d7a4ce4ecb78
SharingWorld <hapsoa@gmail.com>
2018-09-17 20:44:40 +0900

    update

--- src/page/WebCrawler.vue
+++ src/page/WebCrawler.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
     .root
+      .links
+        router-link(to="/test") Go to component
       input(type="text" placeholder="url 입력")
       .button(@click="getSiteContents") Get Site Contents!
       .viewer(v-html="siteContent")
